c816cd297e5d237c38b7940555c06ea150a70704
David Furey <david@furey.me.uk>
2020-05-23 14:43:05 +0100

    Refactor eventlist into separate file

---
+++ src/components/EventList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, ListGroup, ButtonGroup } from 'react-bootstrap'
+import { OSDLiveEvent } from '../reducers/shared';
+import { EditPane, EditPaneType } from '../reducers/editpanel';
+
+function EventListItem(props: { 
+  event: OSDLiveEvent;
+  openTab: (pane: EditPane) => void;
+ }): JSX.Element {
+  function settings(): void {
+    props.openTab({
+      type: EditPaneType.Event,
+      id: props.event.id,
+    })
+  }
+
+  return <ListGroup.Item className="d-flex justify-content-between align-items-center">
+    {props.event.name}
+    <ButtonGroup>
+      <Button size="sm" onClick={settings}><span className="material-icons">settings</span></Button>
+      <Button variant="danger" size="sm"><span className="material-icons">delete</span></Button> {/* todo: bigger bin*/}
+    </ButtonGroup>
+  </ListGroup.Item>
+}
+
+export function EventList(
+  props: { events: OSDLiveEvent[]; openTab: (pane: EditPane) => void }
+): JSX.Element {
+  return <ListGroup>
+    {props.events.map((event) => 
+      <EventListItem key={event.id} event={event} openTab={props.openTab} />
+    )}
+    <ListGroup.Item>
+      <Button>
+        <span className="material-icons material-icons-raised">add</span> New event (todo)
+      </Button>
+    </ListGroup.Item>
+  </ListGroup>
+}
--- src/components/ManageSelectorPanel.tsx
+++ src/components/ManageSelectorPanel.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Button, ListGroup, ButtonGroup } from 'react-bootstrap'
 import { ComponentList } from './ComponentList'
+import { EventList } from './EventList'
 import { OSDLiveEvent } from '../reducers/shared';
 import { OSDComponent } from '../OSDComponent';
 import { TabbedPanel, TabContainer } from './TabbedPanel'
-import { EditPane, EditPaneType } from '../reducers/editpanel';
+import { EditPane } from '../reducers/editpanel';
 
 interface ManageSelectorPanelProps {
   events: { [key: string]: OSDLiveEvent };
@@ -13,35 +13,12 @@ interface ManageSelectorPanelProps {
   openTab: (pane: EditPane) => void;
 }
 
-function EventListItem(props: { 
-  event: OSDLiveEvent;
-  openTab: (pane: EditPane) => void;
- }): JSX.Element {
-  function settings(): void {
-    props.openTab({
-      type: EditPaneType.Event,
-      id: props.event.id,
-    })
-  }
 
-  return <ListGroup.Item className="d-flex justify-content-between align-items-center">
-    {props.event.name}
-    <ButtonGroup>
-      <Button size="sm" onClick={settings}><span className="material-icons">settings</span></Button>
-      <Button variant="danger" size="sm"><span className="material-icons">delete</span></Button> {/* todo: bigger bin*/}
-    </ButtonGroup>
-  </ListGroup.Item>
-}
 
 export function ManageSelectorPanel(props: ManageSelectorPanelProps): JSX.Element {
   return <TabbedPanel>
     <TabContainer name="Event" eventKey="events">
-      <ListGroup>
-        {Object.values(props.events).map((event) => 
-          <EventListItem key={event.id} event={event} openTab={props.openTab} />
-        )}
-        <ListGroup.Item><Button><span className="material-icons material-icons-raised">add</span> New event (todo)</Button></ListGroup.Item>
-      </ListGroup>
+      <EventList events={Object.values(props.events)} openTab={props.openTab} />
     </TabContainer>
     <TabContainer name="Components" eventKey="components">
       <ComponentList 
