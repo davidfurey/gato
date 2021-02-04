0f81cd098d7e33e3f9b62414c8cc230eef556370
David Furey <david@furey.me.uk>
2021-02-04 20:24:55 +0000

    Tidying tabbedpanel

--- src/components/ui/TabbedPanel.tsx
+++ src/components/ui/TabbedPanel.tsx
@@ -31,16 +31,52 @@ function isTabContainer(node: React.ReactNode): node is TabContainer {
     tabContainer.props.eventKey !== undefined
 }
 
+function CloseButton(props: { closeTab: () => void }): JSX.Element {
+  return <Button
+    onClick={props.closeTab}
+    className="close mr-n3 ml-3"
+    aria-label="Close"
+  ><span aria-hidden="true">&times;</span></Button>
+}
+
+function NavItem(props: TabContainerProps & { variant: "pills" | "tabs", size?: "sm" }): JSX.Element {
+  const style = props.variant === "pills" ? {
+    navItemClass: "d-flex justify-content-center",
+    navLinkClass: "btn btn-primary px-1 d-flex justify-content-center" + (props.size === "sm" ? " btn-sm" : ""),
+    bsPrefix: "custom-nav-link"
+  } : {
+    navItemClass: "d-flex justify-content-between align-items-center",
+    navLinkClass: "d-flex justify-content-between align-items-center",
+    bsPrefix: undefined
+  }
+
+  return <Nav.Item className={style.navItemClass}>
+    <Nav.Link
+      eventKey={props.eventKey}
+      className={style.navLinkClass}
+      bsPrefix={style.bsPrefix}
+    >
+      {props.name}
+      {props.closeTab ? <CloseButton closeTab={props.closeTab} /> : null }
+    </Nav.Link>
+  </Nav.Item>
+}
+
 export function TabbedPanel(props: TabbedPanelProps): JSX.Element {
   const firstTab = React.Children.toArray(props.children).filter(isTabContainer)[0]
-  const defaultActiveKey = props.defaultActiveKey || ( firstTab ? firstTab.props.eventKey : "")
+  const defaultActiveKey = props.defaultActiveKey || firstTab?.props.eventKey
 
   const onSelect = props.onSelect
   const safeOnSelect = onSelect ?
     ((eventKey: string | null) => eventKey ? onSelect(eventKey) : null) : undefined
 
+  const variant = props.variant || "tabs"
+
+  const navClass = props.variant === "pills" ?
+    "text-center justify-content-center" :
+    "text-center"
+
   return <Tab.Container
-    id="left-tabs-example"
     activeKey={props.activeKey}
     defaultActiveKey={defaultActiveKey}
     transition={false}
@@ -48,30 +84,13 @@ export function TabbedPanel(props: TabbedPanelProps): JSX.Element {
   >
     <Card className={props.className === undefined ? "mb-3" : props.className}>
       <Card.Header className={props.size === "sm" ? "px-3 py-2 bg-primary" : ""}>
-        { props.variant === "pills" ?
-        <Nav variant={props.variant || "tabs"} className="text-center justify-content-center">
+        <Nav variant={variant} className={navClass}>
         { React.Children.map(props.children, (child) =>
           isTabContainer(child) ?
-            <Nav.Item className="d-flex justify-content-center">
-              <Nav.Link eventKey={child.props.eventKey} bsPrefix="custom-nav-link" className={"btn btn-primary px-1 d-flex justify-content-center" + (props.size === "sm" ? " btn-sm" : "")}>
-                {child.props.name}
-                {child.props.closeTab ? <Button onClick={child.props.closeTab} className="close mr-n3 ml-3" aria-label="Close"><span aria-hidden="true">&times;</span></Button> : null}
-              </Nav.Link>
-            </Nav.Item> : null
-        )}
-        </Nav> :
-        <Nav variant={props.variant || "tabs"} className="text-center">
-        { React.Children.map(props.children, (child) =>
-          isTabContainer(child) ?
-            <Nav.Item className="d-flex justify-content-between align-items-center">
-              <Nav.Link eventKey={child.props.eventKey} className="d-flex justify-content-between align-items-center">
-                {child.props.name}
-                {child.props.closeTab ? <Button onClick={child.props.closeTab} className="close mr-n3 ml-3" aria-label="Close"><span aria-hidden="true">&times;</span></Button> : null}
-              </Nav.Link>
-            </Nav.Item> : null
+            <NavItem {...child.props} size={props.size} variant={variant} /> :
+            null
         )}
         </Nav>
-        }
       </Card.Header>
       <Tab.Content>{props.children}</Tab.Content>
     </Card>
