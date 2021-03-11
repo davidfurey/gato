ca4e5ebfd53abe75c4f562ffa85737ee2336b106
David Furey <david@furey.me.uk>
2021-03-11 20:52:10 +0000

    Add superscript ordinals (st, nd, th) to numbers on slides

--- src/components/OSDComponents/Slide.tsx
+++ src/components/OSDComponents/Slide.tsx
@@ -21,15 +21,26 @@ interface SlideProps {
   subtitle: string;
 }
 
+function withOrdinals(s: string): JSX.Element {
+  const index = s.search(/(1st|2nd|3rd|[0,4-9]th)/)
+  if (index >= 0) {
+    return <>
+      {s.slice(0, index + 1)}
+      <sup>{s.slice(index + 1, index + 4)}</sup>
+      {withOrdinals(s.slice(index + 4))}
+    </>
+  }
+  return <>{s}</>
+}
 function Slide(props: SlideProps): JSX.Element {
   const customClassName = props.className ? ` ${props.className}` : ""
   const className = props.state === "entering" || props.state === "visible" ?  "slide-component slide-component-visible" : "slide-component slide-component-hidden"
-  const subtitle = props.subtitle.split('\n').map ((item, i) => <p key={i}>{item}</p>)
+  const subtitle = props.subtitle.split('\n').map ((item, i) => <p key={i}>{withOrdinals(item)}</p>)
   return <div className={className + customClassName}>
     <div className="image" style={{top: props.top, left: props.left}}>
       <img alt="" src={props.src} width={props.width} height={props.height} />
     </div>
-    <div className="title">{props.title}</div>
+    <div className="title">{withOrdinals(props.title)}</div>
     <div className="subtitle">{subtitle}</div>
     <div className="extra1"><span></span></div>
     <div className="extra2"><span></span></div>
