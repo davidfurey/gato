a41ba5854c1b24b0f2b240c4ee1e5f2f7cc57b56
David Furey <david@furey.me.uk>
2021-04-06 19:08:49 +0100

    Allow uploading and downloading themes and styles

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { ListGroup, ListGroupItem, Col, DropdownButton, Dropdown, Row, ButtonGroup, ButtonToolbar } from 'react-bootstrap'
-import { ComponentType, OSDLiveEvents, Settings, Style, Styles, Theme, Themes, } from '../reducers/shared'
-import { CollapsablePanel } from './ui'
+import React, { useState } from 'react';
+import { ListGroup, ListGroupItem, Col, DropdownButton, Dropdown, Row, ButtonGroup, Button, Modal, Form, Alert } from 'react-bootstrap'
+import { ComponentType, OSDLiveEvents, Settings, Style, Styles, Themes, } from '../reducers/shared'
+import { CollapsablePanel, IconButton } from './ui'
 import { componentTypes, componentTypeAsString } from './OSDComponents';
 
 export interface SettingsPanelProps {
@@ -38,6 +38,81 @@ function StyleSelector(props: {
   </Col>
 }
 
+function ImportExport(props: {
+  import: string;
+  export: string;
+  name: string;
+}): JSX.Element {
+  const [show, setShow] = useState(false);
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [error, setError] = useState(false);
+
+  const handleClose = (): void => {
+    setFile(undefined)
+    setError(false)
+    setShow(false)
+  }
+
+  const handleShow = (): void => setShow(true)
+
+  const handleUpload = (): void => {
+    setError(false)
+    if (file !== undefined) {
+      fetch(props.import, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: file
+      }).then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          handleClose()
+        } else {
+          setError(true)
+        }
+      }).catch((error) => {
+        setError(true)
+        console.log(error)
+      })
+     } else {
+       console.warn("Attempted to upload null file")
+     }
+  };
+
+  return <ListGroup.Item className="d-flex justify-content-between align-items-center pl-5 border-bottom-0">
+    { props.name }
+    <ButtonGroup size="sm">
+      <IconButton variant="danger" onClick={handleShow} icon="file_upload">Import</IconButton>
+      <IconButton variant="info" icon="file_download" href={props.export} download>Export</IconButton>
+    </ButtonGroup>
+    <Modal show={show} onHide={handleClose} animation={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Import { props.name.toLowerCase() }</Modal.Title>
+      </Modal.Header>
+        <Modal.Body>
+          <Alert variant="danger">This will overwrite existing { props.name.toLowerCase() } with the same id</Alert>
+          { error ? <Alert variant="warning">An error occured while uploading. Please try again</Alert> : null }
+          <Form.Group>
+              <Form.File
+                accept="application/json"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setFile(event.target.files ? event.target.files[0] : undefined)
+                }}
+              />
+          </Form.Group>
+        </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={handleUpload} disabled={file === undefined}>
+          Upload
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </ListGroup.Item>
+}
+
 export function SettingsPanel(props: SettingsPanelProps): JSX.Element {
   return (
     <CollapsablePanel header="Settings" open={false}>
@@ -56,6 +131,9 @@ export function SettingsPanel(props: SettingsPanelProps): JSX.Element {
           componentType={componentType}
         /></Row>
         </ListGroupItem>})}
+        <ListGroup.Item className="text-light font-weight-bold border-top border-secondary border-bottom-0 mt-2 pb-1">Import/Export</ListGroup.Item>
+        <ImportExport import="/api/import/styles" export="/api/export/styles" name="Styles" />
+        <ImportExport import="/api/import/themes" export="/api/export/themes" name="Themes" />
       </ListGroup>
     </CollapsablePanel>
   )
--- src/server/api.ts
+++ src/server/api.ts
@@ -3,7 +3,9 @@ import express from 'express';
 import { Message } from '../api/Messages'
 import * as Event from '../api/Events'
 import * as Transition from '../api/Transitions'
-import { OSDLiveEvent, SharedState, Display } from '../reducers/shared'
+import * as ThemeMessage from '../api/Themes'
+import * as StyleMessage from '../api/Styles'
+import { OSDLiveEvent, SharedState, Display, Themes, Styles } from '../reducers/shared'
 import { OSDComponent } from '../OSDComponent';
 import { createIs } from 'typescript-is';
 import { notEmpty } from '../api/FunctionalHelpers';
@@ -60,4 +62,54 @@ export function createApiRoutes(
 
   routes.collection('/displays', displays, fields(["name", "id"]))
   routes.message('/displays/:displayId/transistion', createIs<Transition.GoTransistion>(), Transition.MessageType.Go)
+
+  app.get(`${routes.prefix}/export/themes`, (_, res) => {
+    res.type('application/json')
+      .attachment('themes.json')
+      .send(JSON.stringify(state().themes))
+  })
+
+  app.get(`${routes.prefix}/export/styles`, (_, res) => {
+    res.type('application/json')
+      .attachment('styles.json')
+      .send(JSON.stringify(state().styles))
+  })
+
+  const isThemes = createIs<Themes>()
+
+  app.post(`${routes.prefix}/import/themes`, (req, res) => {
+    const body: unknown = req.body
+    if (typeof body === 'object' && isThemes(body)) {
+      Object.values(body).forEach((theme) => {
+        const create: ThemeMessage.Create = {
+          type: ThemeMessage.MessageType.Create,
+          id: theme.id,
+          theme
+        }
+        processMessage(create)
+      })
+      res.status(202).send("Accepted")
+    } else {
+      res.status(400).send("Invalid request")
+    }
+  })
+
+  const isStyles = createIs<Styles>()
+
+  app.post(`${routes.prefix}/import/styles`, (req, res) => {
+    const body: unknown = req.body
+    if (typeof body === 'object' && isStyles(body)) {
+      Object.values(body).forEach((style) => {
+        const create: StyleMessage.Create = {
+          type: StyleMessage.MessageType.Create,
+          id: style.id,
+          style
+        }
+        processMessage(create)
+      })
+      res.status(202).send("Accepted")
+    } else {
+      res.status(400).send("Invalid request")
+    }
+  })
 }
